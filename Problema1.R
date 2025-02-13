0b21fca4d2bfdf4f595d71ca1a3d63108ebb6847
martin.salva <martin.salva@auleseebe.upc.edu>
2025-02-13 13:00:19 +0100

    ....

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,31 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+
+#1
+mean(mis_dades$qsec)
+
+x <- mis_dades$qsec
+
+sum(x)/length(x)
+
+#2
+x <- mis_dades$drat
+sort(x)
+median(x)
+#3
+quantile(x, 0,25)
+
+#4
+quantile(mis_dades$mpg, 0.18)
+
+#5
+quantile(mis_dades$cyl, 0.75)-quantile(mis_dades$cyl, 0.25)
+#és el mateix que:
+IQR(mis_dades$cyl)
+
+#6
+sd(mis_dades$cyl)
+
+#7
+var(mis_dades$cyl)
