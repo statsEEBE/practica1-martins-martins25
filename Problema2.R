9c2799e45c41d252d6496cec461ea13634bdb1e6
martin.salva <martin.salva@auleseebe.upc.edu>
2025-02-13 13:43:05 +0100

    ......

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,35 @@
 #Codigo para problema 2
+rm(list=ls())
+data <- mtcars
+
+#discretas
+x <- data$cyl
+ni <- table(x)
+fi <- ni/length(x)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni,fi,Ni,Fi)
+
+#continuos
+x <- data$mpg
+bins <- cut(x,10)
+bins
+ni <- table(bins)
+fi <- ni/length(bins)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni,fi,Ni,Fi)
+
+#histograma: continuas
+hist(data$mpg)
+
+#barplot: discretas
+x <- data$cyl
+ni <- table(x)
+barplot(ni)
+
+#pie: discretas
+x <- data$cyl
+ni <- table(x)
+fi <- ni/length(x)
+pie(fi)
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,7 @@
 #Codigo para problema 3
+#el resoldrem amb un nou vector per no copiar el de l'encunciat
+x <- c(8, 6, 0, 1, 7, 5, 8, 1, 3, 4)
+sum(x)
+
+y <- x[-c(3,7,8)]
+res <- sum(exp(x))-sum(exp(y))
